228d21bef404ac737177559508627c0c07ab40a2
Babatunde Olajide <obabs78@gmail.com>
2020-06-16 19:30:33 -0500

    A great start

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -5,7 +5,7 @@ const Contact = mongoose.model('Contact', ContactSchema);
 
 export const addNewContact = (req, res) => {
     let newContact = new Contact(req.body)
-
+    // save new contact
     newContact.save((err, contact) => {
         if (err) {
             res.send(err)
