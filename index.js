8eb4843853f735a4bddde7cf1d777a857bb22b04
Babatunde Olajide <obabs78@gmail.com>
2020-06-16 22:40:20 -0500

    Update to index.js

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ import express from 'express';
 import mongoose from 'mongoose';
 import routes  from './src/routes/crmRoutes'
 import bodyParser from 'body-parser'
+import helmet from 'helmet'
+import RateLimit from 'express-rate-limit'
+import cookieParser from 'cookie-parser'
+import csrf from 'csurf'
 
 const app = express();
 const PORT = 4000;
+
+//setup helmet for security
+app.use(helmet())
+
+// we use cookieParser becaue cookie is set to true in csrf protection
+app.use(cookieParser())
+
+// csrf middleware for XSS forgery protection suppose we have forms
+const csrfProtection = csrf({cookie:true})
+
+//setup rate limit to prevent DOS attack
+const limiter = new RateLimit({
+    windowMs:15*60*1000, // 15 minutes
+    max:100, // maximum number of request from an IP in 15 minues
+    delayMs:0 // disables delays
+})
+
+app.use(limiter())
 
 // mongoose connection
 mongoose.Promise = global.Promise;
@@ -17,6 +39,9 @@ mongoose.connect('mongodb://localhost/CRMdb', {
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+// bodyParser for form only
+const parseForm = bodyParser.urlencoded({extended:false})
+
 // add routes to app
 routes(app);
 
@@ -26,5 +51,16 @@ app.use(express.static('public'))
 app.get('/', (req, res) => (
     res.send(` Node and express server running on port ${PORT}`)
 ));
+
+// parse a form and inject a csrf token using the middleware
+app.get('/form',csrfProtection, (req, res) => {
+    //pass the token to the form view
+    res.render('send', {csrfToken: req.csrfToken()})
+})
+
+// to process the form
+app.post('/process', parseForm, csrfProtection, (req, res) => {
+    res.send('form data is being processed')
+})
 
 app.listen(PORT, () => console.log(`APP is listening on port ${PORT}`));
